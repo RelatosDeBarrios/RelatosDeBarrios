323ef88121e0a19f888bfc51784c31f6846029e2
Strocs <strocsdev@gmail.com>
2025-08-10 01:33:01 -0400

    feat(api): add cleanup blobs endpoint for cron job with auth

--- app/(hub)/api/cleanup-blobs/route.ts
+++ app/(hub)/api/cleanup-blobs/route.ts
@@ -1,2 +1,26 @@
-// Delete unused blobs every 1 day in a cron job
-export async function GET() {}
+import { NextResponse } from 'next/server'
+import { del, list } from '@vercel/blob'
+
+export async function GET(request: Request): Promise<NextResponse> {
+  // Authentication check for the cron job
+  const authHeader = request.headers.get('Authorization')
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`)
+    // 401 Unauthorized
+    return new NextResponse(null, { status: 401 })
+
+  try {
+    const { blobs } = await list()
+    const blobsToDelete = blobs.map((b) => b.url)
+
+    if (blobsToDelete.length === 0)
+      return new NextResponse(null, { status: 204 })
+
+    del(blobsToDelete)
+
+    // 204 No Content
+    return new NextResponse(null, { status: 204 })
+  } catch {
+    // 500 Internal Server Error
+    return new NextResponse(null, { status: 500 })
+  }
+}
