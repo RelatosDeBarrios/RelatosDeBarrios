b2623f9a4ba6bf52eb9fadb5cf0249217f695d2e
Strocs <strocsdev@gmail.com>
2025-09-08 20:02:19 -0300

    refactor(villacovico): update main page to use new hero and visual archive

--- app/villacovico/(pages)/page.tsx
+++ app/villacovico/(pages)/page.tsx
@@ -1,9 +1,11 @@
-import Hero from '../sections/hero/hero'
+import Hero from '@/covico/sections/hero/hero'
+import { VisualArchive } from '../sections/visual-archive/visual-archive'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col gap-10 px-10 pb-10'>
+    <main className='flex min-h-screen flex-col gap-26 px-10 pb-10'>
       <Hero />
+      <VisualArchive />
       <div className='h-screen w-full bg-red-200'></div>
       <div className='h-screen w-full bg-blue-200'></div>
       {/* <PhotoArchive /> */}
