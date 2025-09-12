052f0d8222ada9403e640460c66046eeeb400547
Strocs <strocsdev@gmail.com>
2025-09-12 15:14:20 -0300

    refactor(villacovico): actualizar imports y estructura de página principal

--- app/villacovico/(pages)/page.tsx
+++ app/villacovico/(pages)/page.tsx
@@ -1,15 +1,20 @@
 import Hero from '@/covico/sections/hero/hero'
-import { VisualArchive } from '../sections/visual-archive/visual-archive'
+import { VisualArchive } from '@/covico/sections/visual-archive/visual-archive'
+import { Footer } from '@/covico/sections/footer/footer'
+import Quote from '../sections/quote/quote'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col gap-26 px-10 pb-10'>
-      <Hero />
-      <VisualArchive />
-      <div className='h-screen w-full bg-red-200'></div>
-      <div className='h-screen w-full bg-blue-200'></div>
-      {/* <PhotoArchive /> */}
-      {/* <Footer /> */}
-    </main>
+    <>
+      <main className='flex min-h-screen flex-col gap-26 px-10 pb-10'>
+        <Hero />
+        <VisualArchive />
+        <Quote position={1} />
+        <div className='h-screen w-full bg-red-200'></div>
+        <div className='h-screen w-full bg-blue-200'></div>
+        {/* <PhotoArchive /> */}
+      </main>
+      <Footer />
+    </>
   )
 }
