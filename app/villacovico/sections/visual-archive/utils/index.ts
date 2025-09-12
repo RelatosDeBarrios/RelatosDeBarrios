c776ffd9096e82313e6e926ba332c2246068e921
Strocs <strocsdev@gmail.com>
2025-09-12 15:15:00 -0300

    chore(villacovico/visual-archive): eliminar utilidades circulares redundantes

--- app/villacovico/sections/visual-archive/utils/index.ts
+++
@@ -1,7 +0,0 @@
-// implement modulo operator to get next and previous index in a circular array
-export const getNextIndex = (currentIndex: number, length: number) => {
-  return (currentIndex + 1) % length
-}
-export const getPrevIndex = (currentIndex: number, length: number) => {
-  return (currentIndex - 1 + length) % length
-}
