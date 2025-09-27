8024b03b8b2e1b2d06ccea3f2a31d3b3b3324c01
Strocs <strocsdev@gmail.com>
2025-09-27 19:04:23 -0300

    refactor(villacovico/pages): integrar sección plans en la página principal

--- app/villacovico/(pages)/page.tsx
+++ app/villacovico/(pages)/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/covico/sections/hero/hero'
 import { VisualArchive } from '@/covico/sections/visual-archive/visual-archive'
 import { Footer } from '@/covico/sections/footer/footer'
 import Quote from '../sections/quote/quote'
+import Plans from '../sections/plans/plans'
 
 export default function Home() {
   return (
@@ -10,9 +11,13 @@ export default function Home() {
         <Hero />
         <VisualArchive />
         <Quote position={1} />
+        <Plans />
+        <Quote position={2} />
+        {/* 3D */}
         <div className='h-screen w-full bg-red-200'></div>
+        <Quote position={1} />
+        {/* Book & Documental */}
         <div className='h-screen w-full bg-blue-200'></div>
-        {/* <PhotoArchive /> */}
       </main>
       <Footer />
     </>
