6545e3776bcd6c5fd3257f27894dfa54a061a25d
Strocs <strocsdev@gmail.com>
2025-09-30 02:16:18 -0300

    refactor(villacovico/visual-archive): update gallery integration

--- app/villacovico/sections/visual-archive/components/VisualArchiveGallery.tsx
+++ app/villacovico/sections/visual-archive/components/VisualArchiveGallery.tsx
@@ -1,14 +1,11 @@
 import { Gallery } from '@/app/villacovico/feature/gallery/gallery'
 import { VisualArchiveGalleryItems } from '../types'
 import { ImageType } from '@/types/general'
-import { useVisualArchiveGallery } from '../hooks/useVisualArchiveGallery'
 
 interface VisualArchiveGalleryProps {
   galleries: Record<VisualArchiveGalleryItems, ImageType[]>
 }
 
 export const VisualArchiveGallery = ({ galleries }: VisualArchiveGalleryProps) => {
-  const store = useVisualArchiveGallery()
-
-  return <Gallery store={store} galleries={galleries} />
+  return <Gallery galleries={galleries} />
 }
--- app/villacovico/sections/visual-archive/hooks/useVisualArchiveGallery.ts
+++ app/villacovico/sections/visual-archive/hooks/useVisualArchiveGallery.ts
@@ -1,6 +1,4 @@
-import { createGalleryAdapter } from '@/covico/feature/gallery/store/createGalleryAdapter'
-import { useCovicoGallery } from '@/covico/feature/gallery/store/CovicoGalleryStore'
+import { createCovicoGallery } from '@/app/villacovico/feature/gallery/store/covicoGalleryStore'
 import type { VisualArchiveGalleryItems } from '../types'
 
-export const useVisualArchiveGallery =
-  createGalleryAdapter<VisualArchiveGalleryItems>(useCovicoGallery)
+export const useVisualArchiveGallery = createCovicoGallery<VisualArchiveGalleryItems>()
