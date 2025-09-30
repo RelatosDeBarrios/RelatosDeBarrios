d4030916c877bf8fbd3d1390c3411d18f2466401
Strocs <strocsdev@gmail.com>
2025-09-30 02:15:45 -0300

    refactor(rengifo): migrate gallery store to use shared factory

--- app/conjuntohabitacionalrengifo/store/galleryStore.ts
+++ app/conjuntohabitacionalrengifo/store/galleryStore.ts
@@ -1,36 +1,3 @@
-import { create } from 'zustand'
+import { createGalleryStore } from '@/hooks/createGalleryStore'
 
-export interface GalleryImage {
-  src: string
-  alt: string
-  width: number
-  height: number
-}
-
-interface GalleryState {
-  isOpen: boolean
-  currentGalleryId: string
-  currentIndex: number
-  openGallery: (galleryId: string, startIndex?: number) => void
-  closeGallery: () => void
-  setIndex: (index: number) => void
-}
-
-export const useGalleryStore = create<GalleryState>((set) => ({
-  isOpen: false,
-  currentGalleryId: '',
-  currentIndex: 0,
-  openGallery: (galleryId, startIndex = 0) =>
-    set({
-      isOpen: true,
-      currentGalleryId: galleryId,
-      currentIndex: startIndex,
-    }),
-  closeGallery: () =>
-    set({
-      isOpen: false,
-      currentGalleryId: '',
-      currentIndex: 0,
-    }),
-  setIndex: (index) => set({ currentIndex: index }),
-}))
+export const useGalleryStore = createGalleryStore()
