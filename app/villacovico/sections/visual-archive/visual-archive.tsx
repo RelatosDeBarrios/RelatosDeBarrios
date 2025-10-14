4a8b4c80f75e671e25163cb99310962c62584f1d
Strocs <strocsdev@gmail.com>
2025-10-14 18:29:13 -0300

    style(covico/visual-archive): update visual-archive layout and spacing for improved responsiveness

--- app/villacovico/sections/visual-archive/visual-archive.tsx
+++ app/villacovico/sections/visual-archive/visual-archive.tsx
@@ -10,17 +10,22 @@ export const VisualArchive = () => {
   const completeGallery = getGallery(VISUAL_ARCHIVE.cards, 'gallery')
 
   return (
-    <article id={VISUAL_ARCHIVE.id} className='relative grid w-full grid-cols-2 gap-x-8'>
+    <article
+      id={VISUAL_ARCHIVE.id}
+      className='grod-rows-2 relative grid w-full gap-x-8 xl:grid-cols-2'
+    >
       <section className='flex flex-col items-end justify-center gap-4'>
         <Title heading='h2' size='xl' weight='semibold' className='text-right'>
           {VISUAL_ARCHIVE.title}
         </Title>
-        <Paragraph className='max-w-[250px] text-right'>{VISUAL_ARCHIVE.subTitle}</Paragraph>
+        <Paragraph className='max-w-[200px] pb-4 text-right md:max-w-[250px]'>
+          {VISUAL_ARCHIVE.subTitle}
+        </Paragraph>
       </section>
 
       <section className='relative'>
         <VisualArchivePoster gallery={completeGallery} />
-        <nav className='absolute top-8 right-8 flex flex-col items-end gap-4'>
+        <nav className='absolute top-4 right-4 flex flex-col items-end gap-2 md:top-8 md:right-8 md:gap-4'>
           {Object.values(VISUAL_ARCHIVE.cards).map((card) => (
             <VisualArchiveSelector key={card.id} id={card.id} archiveTitle={card.title} />
           ))}
