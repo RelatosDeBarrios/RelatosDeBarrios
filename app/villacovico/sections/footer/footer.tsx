db8ebd81db36416724f8754cdf52ebf4b90d0b43
Strocs <strocsdev@gmail.com>
2025-10-14 18:27:17 -0300

    style(covico/footer): adjust padding, scaling, and text layout for footer

--- app/villacovico/sections/footer/footer.tsx
+++ app/villacovico/sections/footer/footer.tsx
@@ -4,7 +4,7 @@ import { NAV } from '@/app/villacovico/sections/nav/content'
 
 export const Footer = () => {
   return (
-    <footer className='relative w-full p-10'>
+    <footer className='relative w-full p-4 xl:p-10'>
       <figure className='relative aspect-square w-full overflow-hidden rounded-xl'>
         <Image
           src='/villacovico/footer.webp'
@@ -14,33 +14,33 @@ export const Footer = () => {
           className='absolute aspect-square w-full'
         />
       </figure>
-      <section className='absolute right-0 bottom-40 left-0 z-10 w-full'>
+      <section className='absolute right-0 bottom-8 left-0 z-10 w-full xl:bottom-40'>
         <div className='mx-auto w-fit'>
-          <HeaderLogo logo={NAV.utilityImages.logo} className='mx-auto w-fit scale-150' />
-          <div className='mx-auto mt-10 flex items-center justify-center gap-8'>
+          <HeaderLogo logo={NAV.utilityImages.logo} className='mx-auto w-fit xl:scale-150' />
+          <div className='mx-auto flex items-center justify-center gap-2 xl:mt-10 xl:gap-8'>
             <Image
               src='/villacovico/1.png'
               alt=''
-              className='h-50 w-fit'
+              className='h-18 w-fit xl:h-50'
               width={568}
               height={567}
             />
             <Image
               src='/villacovico/2.png'
               alt=''
-              className='h-30 w-fit'
+              className='h-10 w-fit xl:h-30'
               width={2498}
               height={995}
             />
             <Image
               src='/villacovico/3.png'
               alt=''
-              className='h-40 w-fit'
+              className='h-12 w-fit xl:h-40'
               width={7729}
               height={3300}
             />
           </div>
-          <p className='text-covico-background pt-20 text-center'>
+          <p className='text-covico-background max-w-sm text-center xl:max-w-full xl:pt-20'>
             Relatos de Barrios 2025 | Desarrollado por StrocsDev
           </p>
         </div>
