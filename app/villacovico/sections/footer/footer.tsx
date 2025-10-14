e90c0035b32ce1ce9772109fcbc5997bb0e24b06
Strocs <strocsdev@gmail.com>
2025-10-14 00:58:30 -0300

    feat(covico/footer): update footer section

--- app/villacovico/sections/footer/footer.tsx
+++ app/villacovico/sections/footer/footer.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { HeaderLogo } from '../nav/components/HeaderLogo'
+import { NAV } from '@/app/villacovico/sections/nav/content'
 
 export const Footer = () => {
   return (
@@ -12,6 +14,37 @@ export const Footer = () => {
           className='absolute aspect-square w-full'
         />
       </figure>
+      <section className='absolute right-0 bottom-40 left-0 z-10 w-full'>
+        <div className='mx-auto w-fit'>
+          <HeaderLogo logo={NAV.utilityImages.logo} className='mx-auto w-fit scale-150' />
+          <div className='mx-auto mt-10 flex items-center justify-center gap-8'>
+            <Image
+              src='/villacovico/1.png'
+              alt=''
+              className='h-50 w-fit'
+              width={568}
+              height={567}
+            />
+            <Image
+              src='/villacovico/2.png'
+              alt=''
+              className='h-30 w-fit'
+              width={2498}
+              height={995}
+            />
+            <Image
+              src='/villacovico/3.png'
+              alt=''
+              className='h-40 w-fit'
+              width={7729}
+              height={3300}
+            />
+          </div>
+          <p className='text-covico-background pt-20 text-center'>
+            Relatos de Barrios 2025 | Desarrollado por StrocsDev
+          </p>
+        </div>
+      </section>
     </footer>
   )
 }
